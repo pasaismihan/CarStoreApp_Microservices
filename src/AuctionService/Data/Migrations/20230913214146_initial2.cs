0af74f8f39cae4b90bd09ecdcab794598cbcf97c
pasaismihan <pasa_13142@hotmail.com>
2023-09-14 16:45:45 +0300

    auction get controllers done

---
+++ src/AuctionService/Data/Migrations/20230913214146_initial2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReversePrice",
+                table: "Auctions",
+                newName: "ReservePrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservePrice",
+                table: "Auctions",
+                newName: "ReversePrice");
+        }
+    }
+}
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,5 @@
 using AuctionService;
+using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -19,5 +20,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    
+    Console.WriteLine(e);
+}
 
 app.Run();
