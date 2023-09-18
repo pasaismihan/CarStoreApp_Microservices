8dd7808245c4cd4c0a99aaf6717a284579fe0ba8
pasaismihan <pasa_13142@hotmail.com>
2023-09-18 17:05:19 +0300

    Search Service FilterBy and OrderBy done

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Entities;
+
+namespace SearchService;
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+[HttpGet]
+public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams){
+    var query = DB.PagedSearch<Item, Item>();
+  
+    // we are sorting searchTerm with Item.Make(like ford) 
+
+if(!string.IsNullOrEmpty(searchParams.searchTerm)){
+    query.Match(Search.Full,searchParams.searchTerm).SortByTextScore();
+}
+
+query = searchParams.OrderBy switch {
+"make" => query.Sort(x => x.Ascending(a => a.Make)),
+"new" => query.Sort(x => x.Descending(a => a.CreateAt)),
+_ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+
+};
+
+query = searchParams.FilterBy switch 
+{
+    "finished" => query.Match(x=>x.AuctionEnd < DateTime.UtcNow),
+    "endingsoon" => query.Match(x=>x.AuctionEnd<DateTime.UtcNow.AddHours(6)
+    && x.AuctionEnd > DateTime.UtcNow),
+    _=> query.Match(x=>x.AuctionEnd > DateTime.UtcNow),
+};
+
+if(!string.IsNullOrEmpty(searchParams.Seller)) {
+    query.Match(x=>x.Seller == searchParams.Seller);
+}
+if(!string.IsNullOrEmpty(searchParams.Winner)) {
+    query.Match(x=>x.Winner == searchParams.Winner);
+}
+
+query.PageNumber(searchParams.PageNumber);
+query.PageSize(searchParams.pageSize);
+
+    var result = await query.ExecuteAsync();
+// it returns List , cause we changed List<Item>
+    return Ok(new {
+        result = result.Results,
+        pageCount = result.PageCount,
+        totalCount = result.TotalCount
+    });
+}
+}
