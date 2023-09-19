f995a8d125f640b6a60a4473ea49ca7802fff7a7
pasaismihan <pasa_13142@hotmail.com>
2023-09-19 17:12:17 +0300

    Configured the connection with AuctionService

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
 
 var app = builder.Build();
 
