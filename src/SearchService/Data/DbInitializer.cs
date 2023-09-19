3e8dd40a0a902624f0d4af45ae22d5d8da417aaa
pasaismihan <pasa_13142@hotmail.com>
2023-09-19 17:35:55 +0300

    GetItemsForSearchDb added on SearchService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -18,13 +18,11 @@ await DB.Index<Item>()
 
 var count = await DB.CountAsync<Item>();
 
-if(count== 0){
-    Console.WriteLine("No Data");
-    var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-    var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-    var items = JsonSerializer.Deserialize<List<Item>>(itemData,options);
+using var scope = app.Services.CreateScope();
+var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+var items = await httpClient.GetItemsForSearchDb();
+Console.WriteLine(items.Count + "returned from the auction service");
 
-    await DB.SaveAsync(items);
-}
+if(items.Count >0) await DB.SaveAsync(items);
  }
 }
